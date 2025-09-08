e3d80be74e395ad85d7b969f7378d9240fa92c72
fabiankent01 <fabiankent01@gmail.com>
2025-09-08 18:14:53 +0100

    n

--- src/js/product.js
+++ src/js/product.js
@@ -4,22 +4,17 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  // Get current cart (if any)
-  let cart = JSON.parse(localStorage.getItem("so-cart")) || [];
+  let cart = JSON.parse(localStorage.getItem("so-cart"));
 
-  // If nothing in localStorage, start with an empty array
   if (!cart) {
     cart = [];
-  }
-  // If cart is not already an array, wrap it in one
-  else if (!Array.isArray(cart)) {
+  } else if (!Array.isArray(cart)) {
     cart = [cart];
   }
-  //Add nee product
+
   cart.push(product);
 
-  //Save updated cart
-  setLocalStorage("so-cart", product);
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
